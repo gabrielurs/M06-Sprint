934b7b0ab54c619da7f0b32b50ffe64ae2b10dd4
Gabriel <gabrielionuturs@iesebre.com>
2023-09-27 21:19:43 +0200

    cargo datos con datatable

--- Practica3_fetch/script.js
+++ Practica3_fetch/script.js
@@ -1,31 +1,41 @@
-let pokemon;
+console.log("hol");
 
-fetch('https://rickandmortyapi.com/api/character/?limit=10000', {
-    method: 'GET',
-})
-   .then(response => response.json())
-   .then(response => localStorage.setItem("pokemon", JSON.stringify(response)))
+async function getData(){
+    const records = await fetch('https://rickandmortyapi.com/api/character/?limit=10000');
+    const data = await records.json();
+
+    let tab = '';
+
+
+
+    data.results.forEach(function (results) {
+        tab += `<tr>
+            <td>${results.name}</td>
+            <td>${results.status}</td>
+            <td>${results.species}</td>
+        </tr>`
+    });
+
+
+    document.getElementById("tbody").innerHTML = tab;
+
+        $('#taula').DataTable({
+        "data": pokemon.results,
+        "columns": [
+            {"data" : "name"},
+            {"data" : "status"},
+            {"data" : "species"},
+        ]
+    });
+}
+
    
 
-   pokemon = JSON.parse(localStorage.getItem("pokemon"));
 
-   var table = document.querySelector('.table');
-   var tableBody = table.querySelector('tbody');
 
-    for (let i = 0; i < pokemon.results.length; i++) {
-        var row = document.createElement('tr');
-        var nombre = document.createElement('td');
-        var estado = document.createElement('td');
-        var species = document.createElement('td');
-        nombre.textContent = pokemon.results[i].name;
-        estado.textContent = pokemon.results[i].status;
-        species.textContent = pokemon.results[i].species;
 
-        row.appendChild(nombre);
-        row.appendChild(estado);
-        row.appendChild(species);
-        tableBody.appendChild(row);
-    }
+
+
+
 
    
-    console.log(JSON.parse(localStorage.getItem("pokemon")));
