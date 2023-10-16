d8e08f28a6133842903b154dbaa597690b4db3f4
Gabriel <gabrielionuturs@iesebre.com>
2023-10-16 17:57:33 +0200

    He identificat error, en aquest cas es degut a que al eliminar el item de localStorage queda com a null per tant, al carregar la pagina, detecta null y no carrega

--- Practica3_fetch/script.js
+++ Practica3_fetch/script.js
@@ -1,38 +1,23 @@
 
 async function getData(){
     let tab = '';
-
-    if(localStorage == null){
     const records = await fetch('https://rickandmortyapi.com/api/character/?limit=10000');
     json = await records.json(); 
+    
+    if(localStorage == null){
     localStorage.setItem("jsonRick",JSON.stringify(json));
     }
-    
+
 
     let data = JSON.parse(localStorage.getItem("jsonRick"));
 
     
-    
-
-    $("#taula").on("click", "#eliminar", function() {
-        var col = $(this).closest("tr");
-        var pos=col.find("td:eq(0)").html();
-        delete data['results'][pos-1]
-        localStorage.setItem("jsonRick", JSON.stringify(data));
-        $(this).closest("tr").remove();
-     });
-
-
-     console.log(data);
-    
-   
- 
 
     document.getElementById("tbody").innerHTML = tab;
 
-    $('#taula').DataTable().destroy();
-     
-      $('#taula').DataTable({
+
+
+    $('#taula').DataTable({
         "data": data.results,
         "columns": [
             {"data" : "id"},
@@ -44,8 +29,23 @@ async function getData(){
                     return '<button type="button" class="btn btn-primary" id="eliminar">Eliminar</button>';
                 }
             }
-        ]
+        ],
+        "columnDefs": [{
+            "targets": '_all',
+            "defaultContent": ""
+        }]
     });
+
+    $("#taula").on("click", "#eliminar", function() {
+        var col = $(this).closest("tr");
+        var pos=col.find("td:eq(0)").html();
+        delete data['results'][pos-1]
+       
+
+        localStorage.setItem("jsonRick", JSON.stringify(data));
+        $(this).closest("tr").remove();
+     });
+
 }
 
    
